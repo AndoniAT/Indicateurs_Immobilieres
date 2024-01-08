aec653aac98b7e69286b73b9bbe603c82db144a8
AndoniAlonsoTort <andoni.alonso-tort@etu.univ-lehavre.fr>
2024-01-08 10:26:10 +0100

    Ajout test graphql mutationPeriodes

--- api/tests/Api/ImmobiliereApiTest.php
+++ api/tests/Api/ImmobiliereApiTest.php
@@ -3,7 +3,7 @@
 namespace App\Tests\Api;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-
+use App\Entity\SeriesGraph;
 class ImmobiliereApiTest extends ApiTestCase
 {
     public function testGetCollection(): void
@@ -27,11 +27,95 @@ class ImmobiliereApiTest extends ApiTestCase
             'nature_mutation' => 'Sale'
         ]]);
 
-        $this->assertResponseStatusCodeSame(201);
+        $this->assertEquals(201);
     }
 
+    /**
+     * Test Series Graph Graphql
+     */
+    public function testCheckSeriesGraph(): void
+    {
+        $client = static::createClient();
+        $query = '
+        {
+            seriesGraphs {
+                dateMutations
+                price
+            }
+        }
+        ';
+        $headers = [
+            'accept' => 'application/json',
+            'content-type' => 'application/json'
+        ];
+        $response = $client->request('POST', '/graphql', 
+        ['json' => [ 'query' => $query ],
+         'headers' => $headers
+        ]);
+        $data = $response->toArray();
+        $seriesGraphs = $data['data']['seriesGraphs'][0];
+        $price = is_float((float)$seriesGraphs['price']);
+        $date = $seriesGraphs['dateMutations'];
+        $dateTimeObject = \DateTime::createFromFormat('Y-m-d\TH:i:sP', $date);
 
+        $obj = new SeriesGraph();
+        $obj->setDateMutations(new \DateTime($date))
+        ->setPrice($price)
+        ->setId(1);
+        $this->assertInstanceOf(SeriesGraph::class, $obj);
+        $this->assertTrue($price);
+        $this->assertInstanceOf(\DateTime::class, $dateTimeObject);
+        $this->assertTrue($dateTimeObject && $dateTimeObject->format('Y-m-d\TH:i:sP') === $date);
+    }
 
+    public function testCheckMuation(): void
+    {
+        $client = static::createClient();
+        $query = '
+        {
+            mutationsPeriodes(start: "2019-01-01", end: "2023-01-01") {
+                totalVente
+                date
+            }
+        }
+        ';
+        $headers = [
+            'accept' => 'application/json',
+            'content-type' => 'application/json'
+        ];
 
+        $response = $client->request('POST', '/graphql', 
+        ['json' => [ 
+            'query' => $query
+        ],
+         'headers' => $headers
+        ]);
+        $data = $response->toArray();
+        $this->assertIsArray($data['data']['mutationsPeriodes']);
+    }
 
+    public function testCheckVentes(): void
+    {
+        $client = static::createClient();
+        $query = '
+        {
+            ventesRegions {
+              region
+            }
+          }
+        ';
+        $headers = [
+            'accept' => 'application/json',
+            'content-type' => 'application/json'
+        ];
+
+        $response = $client->request('POST', '/graphql', 
+        ['json' => [ 
+            'query' => $query
+        ],
+         'headers' => $headers
+        ]);
+        $data = $response->toArray();
+        $this->assertIsArray($data['data']['ventesRegions']);
+    }
 }
